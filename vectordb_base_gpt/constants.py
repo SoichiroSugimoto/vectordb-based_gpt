f3766c537dac6a001b68918717b0b4c7d1303e0e
杉本創一朗 <fcsoichi1998@gmail.com>
2023-09-04 02:21:45 +0900

    feat: make propmt query from environment variables for debagging

--- vectordb_base_gpt/constants.py
+++ vectordb_base_gpt/constants.py
@@ -1,8 +1,19 @@
-FORMATTED_PROMPT_TEXT = "Language: JP, \
-    Role: Back-Office Expert, \
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+default_formatted_prompt_text = "Language: JP, \
+    Requested-Role: Back-Office Expert, \
     Question: %s, \
-    Reference URL: Required, \
+    History: %s, \
+    Reference URL: Required for completion (Provide Specific Source), \
     No Reference: Skip"
+if os.getenv('FORMATTED_PROMPT_TEXT') is None or \
+        os.getenv('FORMATTED_PROMPT_TEXT').count("%s") != 2:
+    FORMATTED_PROMPT_TEXT = default_formatted_prompt_text
+else:
+    FORMATTED_PROMPT_TEXT = os.environ['FORMATTED_PROMPT_TEXT']
 MESSAGE_PLEASE_WAITING = "The message has been received. \
-    Please wait while we generate your answer: "
+    \nPlease wait while we generate your answer: "
 MESSAGE_ANSWER_UNGENERATED = "Sorry, I don't know the answer."
