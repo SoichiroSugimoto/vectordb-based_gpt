e29c878522a1fcb9fc172a9bcc4439a28d5311f2
杉本創一朗 <fcsoichi1998@gmail.com>
2023-09-04 02:19:58 +0900

    feat: get conversation data with SlackClient Class

--- vectordb_base_gpt/slack_client.py
+++ vectordb_base_gpt/slack_client.py
@@ -1,25 +1,46 @@
 import logging
 import os
+import constants as const
 
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
+
 class SlackClient:
-    def __init__(self, slack_bot_token):
+    def __init__(self, slack_bot_token, received_data):
         self.client = WebClient(token=slack_bot_token)
+        self.content = received_data['event']['text']
+        if 'thread_ts' in received_data['event']:
+            self.ts = received_data['event']['thread_ts']
+        else:
+            self.ts = received_data['event']['ts']
+        self.channel_id = received_data['event']['channel']
+        elements = received_data['event']['blocks'][0]['elements'][0]['elements']
+        self.texts = [element['text'] for element in elements if element['type'] == 'text']
         self.logger = logging.getLogger(__name__)
 
-    def get_thread_text_array(self, channel_id, ts):
+    def get_conversation_as_array(self, ts):
         try:
             result = self.client.conversations_replies(
-                channel=channel_id,
+                channel=self.channel_id,
                 ts=ts
             )
-            thread_text_array = []
-            self.logger.info(result)
-            for message in vars(result)['_initial_data']['messages']:
-                thread_text_array.append(message['text'])
-            return thread_text_array
+            if len(result['messages']) <= 1:
+                return ''
+            else:
+                conversation_data = []
+                for message in result['messages']:
+                    data_list = message['blocks'][0]['elements'][0]['elements']
+                    for item in data_list:
+                        if 'type' in item and item['type'] == 'text':
+                            text = item['text']
+                    if not text.startswith(const.MESSAGE_PLEASE_WAITING):
+                        if 'bot_id' in message.keys():
+                            conversation_data.append({'role': 'Back-Office Expert', 'content': text})
+                        else:
+                            conversation_data.append({'role': 'Client', 'content': text})
+                print(conversation_data)
+                return str(conversation_data)
 
         except SlackApiError as e:
             self.logger.error(f"Error posting message: {e}")
